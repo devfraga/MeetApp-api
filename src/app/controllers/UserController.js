76338c6f80c82d6e43bc4f9f6e6557162eb4982a
Matheus Fraga <devfraga@gmail.com>
2019-07-01 17:50:38 -0300

    Edicao dos dados de um usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,8 +19,36 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    // Como o middleware auth coloca dentro do req o id do user podemos acessar pelo req.userId;
+    // console.log(req.userId);
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Verificando se o email é diferente do que ele esta mandando agora
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      // Caso seja diferente verificando se esse email ja existe em algum usuario
+      if (userExists) {
+        return res.status(400).json({ error: 'Email already existis.' });
+      }
+    }
+
+    // Chegando se o password antigo é igual o novo\
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Passowrd does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      // Aqui devolvemos apenas o id, nome, email e providar quando registra novo usuario
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
